8738e54c9f8c07102bc590d5cf81f3ec0fdbb6cf
Shubham-nebula <129153839+Shubham-nebula@users.noreply.github.com>
2023-07-10 21:08:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def download_file():
     else:
         return jsonify({"message": "File download failed."})
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 def search_questions(questions, context):
     pdf_directory = "./transcripts/"
